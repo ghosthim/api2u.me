f75a285e08614f0609565f2196b69eba48471281
LeeYoungJe <344667237@qq.com>
2022-01-06 14:36:24 +0800

    @giscus/react → JS

--- components/Comments.tsx
+++ components/Comments.tsx
@@ -3,15 +3,19 @@ import { Giscus } from '@giscus/react'
 const Comments = () => {
   return (
     <div id="comments-section" className="border rounded border-gray-400/30 mt-4 p-4 md:-mx-4">
-      <Giscus
-        repo="ghosthim/giscus_discussions"
-        repoId="R_kgDOGnsF1g"
-        category="Announcements"
-        categoryId="DIC_kwDOGnsF1s4CAh9R"
-        mapping="pathname"
-        reactionsEnabled="1"
-        theme="preferred_color_scheme"
-      />
+        <script src="https://giscus.app/client.js"
+              data-repo="ghosthim/giscus_discussions"
+              data-repo-id="R_kgDOGnsF1g"
+              data-category="Announcements"
+              data-category-id="DIC_kwDOGnsF1s4CAh9R"
+              data-mapping="pathname"
+              data-reactions-enabled="1"
+              data-emit-metadata="1"
+              data-theme="preferred_color_scheme"
+              data-lang="en"
+              crossorigin="anonymous"
+              async>
+        </script>
     </div>
   )
 }
